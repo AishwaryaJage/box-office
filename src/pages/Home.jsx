d0e1f6fd51b5195cefe2a44ad877dd4003a43d2f
Aishwarya Jage <aishwaryajage@Aishwaryas-MacBook-Air.local>
2022-07-12 00:20:15 +0530

    created preview card for api data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 function Home() {
   const [input, setInput] = useState('');
   const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
 
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
   const onSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
     });
   };
@@ -20,18 +25,18 @@ function Home() {
       onSearch();
     }
   };
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
   const renderResults = () => {
     if (results && results.length === 0) {
       return <div> No Result Found</div>;
     }
     if (results && results.length > 0) {
-      return (
-        <div>
-          {' '}
-          {results.map(item => (
-            <div key={item.show.id}> {item.show.name} </div>
-          ))}
-        </div>
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
       );
     }
     return null;
@@ -44,9 +49,32 @@ function Home() {
         <input
           type="text"
           onChange={onInputChange}
+          placeholder="Search for something..."
           value={input}
           onKeyDown={onKeyDown}
         />
+        <div>
+          <label htmlFor="shows-search">
+            Shows
+            <input
+              type="radio"
+              id="shows-search"
+              value="shows"
+              checked={isShowsSearch}
+              onChange={onRadioChange}
+            />
+          </label>
+          <label htmlFor="actors-search">
+            Actors
+            <input
+              type="radio"
+              id="actors-search"
+              value="people"
+              checked={!isShowsSearch}
+              onChange={onRadioChange}
+            />
+          </label>
+        </div>
         <button type="button" onClick={onSearch}>
           {' '}
           Search{' '}
